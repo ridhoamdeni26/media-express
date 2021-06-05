d39c664eb2e5d6350c3d75e91a0097d20b89c43f
ridhoamdeni26 <secretridho26@gmail.com>
2021-06-06 01:22:04 +0700

    First Commit Service Media Using Express JS

---
+++ routes/media.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const router = express.Router();
+const isBase64 = require('is-base64');
+const base64Img = require('base64-img');
+const fs = require('fs');
+
+const { Media } = require('../models');
+
+// API untuk dapetin data image
+router.get('/', async(req, res) => {
+  const media = await Media.findAll({
+    attributes: ['id', 'image']
+  });
+
+  const mappedMedia = media.map((m) => {
+    m.image = `${req.get('host')}/${m.image}`;
+    return m;
+  })
+
+  return res.json({
+    status: 'success',
+    data: mappedMedia
+  });
+});
+
+
+// API untuk insert data image
+router.post('/', (req, res) => {
+  const image = req.body.image;
+
+  if (!isBase64(image, { mimeRequired: true })) {
+    return res.status(400).json({ status: 'error', message: 'invalid base64' });
+  }
+
+  // check if image isbase6
+  // return res.send('ok');
+
+  base64Img.img(image, './public/images', Date.now(), async (err, filepath) =>{
+    if (err) {
+      return res.status(400).json({ status: 'error', message: err.message });
+    }
+
+    const filename = filepath.split("\\").pop().split("/").pop();
+
+    const media = await Media.create({ image: `images/${filename}` });
+
+    return res.json({
+      status: 'success',
+      data: {
+        id: media.id,
+        image: `${req.get('host')}/images/${filename}`
+      }
+    });
+  })
+});
+
+// API untuk delete image
+router.delete('/:id', async (req, res) => {
+  const id = req.params.id;
+
+  const media = await Media.findByPk(id);
+
+  if (!media){
+    return res.status(404).json({ status: 'error', message: 'Media Not Found'});
+  }
+
+  fs.unlink(`./public/${media.image}`, async (err) => {
+    if (err){
+      return res.status(400).json({ status: 'error', message: err.message});
+    }
+
+    await media.destroy();
+
+    return res.json({
+      status: 'success',
+      message: 'image sucessfully deleted'
+    });
+  });
+});
+
+module.exports = router;
